bd78443edeadbf01302eb501c1a95a7b48f36715
Srushti Lad <srushtilad3001@gmail.com>
2026-08-06 23:46:44 +0530

    Time: 752 ms (13.69%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -2,7 +2,7 @@
 SELECT employee_id
 FROM Employees
 WHERE employee_id NOT IN (SELECT employee_id FROM Salaries)
-UNION
+UNION ALL
 SELECT employee_id
 FROM Salaries
 WHERE employee_id NOT IN (SELECT employee_id FROM Employees)
