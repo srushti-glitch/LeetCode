6c9e41844997a017936cccd828dc0524770be17a
Srushti Lad <srushtilad3001@gmail.com>
2026-08-06 23:45:55 +0530

    Time: 898 ms (5.95%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,15 +1,9 @@
-SELECT e.employee_id
-FROM Employees e
-LEFT JOIN Salaries s
-ON e.employee_id = s.employee_id
-WHERE s.salary IS NULL
-
+# Write your MySQL query statement below
+SELECT employee_id
+FROM Employees
+WHERE employee_id NOT IN (SELECT employee_id FROM Salaries)
 UNION
-
-SELECT s.employee_id
-FROM Salaries s
-LEFT JOIN Employees e
-ON s.employee_id = e.employee_id
-WHERE e.name IS NULL
-
-ORDER BY employee_id;
+SELECT employee_id
+FROM Salaries
+WHERE employee_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY 1;
